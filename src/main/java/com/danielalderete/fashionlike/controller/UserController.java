3c52c763625afd1ca5f329b0689fca2e73f8c78a
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-09-22 11:53:52 -0300

    feat(api): add crud post

---
+++ src/main/java/com/danielalderete/fashionlike/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.danielalderete.fashionlike.controller;
+
+import com.danielalderete.fashionlike.dtos.UserDTO;
+import com.danielalderete.fashionlike.usecases.UserUseCases;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/user")
+@AllArgsConstructor
+public class UserController {
+
+    private UserUseCases useCase;
+
+
+    @PostMapping(path = "/new", produces = "application/json")
+    public ResponseEntity<UserDTO> createPostType(@RequestBody UserDTO userDTO) {
+        UserDTO userCreated = useCase.create(userDTO);
+        return ResponseEntity.ok(userCreated);
+    }
+
+    @PutMapping(path = "/update/{id}", produces = "application/json")
+    public ResponseEntity<UserDTO> updatePostType(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        UserDTO userUpdated = useCase.update(userDTO, id);
+        return ResponseEntity.ok(userUpdated);
+    }
+
+
+    @GetMapping(path = "/all", produces = "application/json")
+    public ResponseEntity<List<UserDTO>> getAllPostTypes() {
+        List<UserDTO> userList = useCase.findAll();
+        return ResponseEntity.ok(userList);
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public ResponseEntity<UserDTO> getPostTypeById(@PathVariable Long id) {
+        UserDTO userFound = useCase.findById(id);
+        return ResponseEntity.ok(userFound);
+    }
+
+
+    @DeleteMapping(path = "/{id}", produces = "application/json")
+    public ResponseEntity<Boolean> deletePostType(@PathVariable Long id) {
+        boolean isDeleted = useCase.delete(id);
+        return ResponseEntity.ok(isDeleted);
+    }
+}
